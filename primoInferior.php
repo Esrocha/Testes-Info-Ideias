a5780142065cee091f593c125032a6fcbbf96cb5
Emanoel Rocha <69223965+Esrocha@users.noreply.github.com>
2021-11-26 18:38:48 -0300

    teste PrioInferior.
    Teste PrimoInferior.

---
+++ primoInferior.php
@@ -0,0 +1,22 @@
+<?php
+
+function primoInferior($num) {
+  
+  $number = $num;
+  $divider = 0;
+    
+    for ($i=2; $i < $number ; $i++) { 
+        if ($number % $i == 0) {
+            $divider++;
+            $number--;
+            if ($number <= 0) {
+                $number = 0;
+            }
+        }  
+    }
+    return " O menor maior número primo abaixo de {$num} é $number";
+   
+  
+}
+
+echo PrimoInferior(10);
